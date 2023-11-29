e77cdf7c07a6e3f985de619679091f3fa12257b4
Gabreal Kalaf <123600253+Gabrealk@users.noreply.github.com>
2023-11-29 12:19:46 -0700

    styling fix

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import 'components/App.css'
 import MovieList from '@/components/MovieList';
 import Headerspace from '@/header';
